a3b1b5bda5841c8af1039a01fe90576478d69122
UyTran <vanuy113@gmail.com>
2018-01-19 10:45:56 +0700

    add box members

--- app/models/user_course.rb
+++ app/models/user_course.rb
@@ -1,6 +1,8 @@
 class UserCourse < ApplicationRecord
   belongs_to :user
   belongs_to :course
+
+  scope :search_course, ->(key){eager_load(:course).where("courses.name LIKE ?", "%#{key}%")}
   enum status: {joined: 0, in_progress: 1, finished: 2}
   delegate :description, :name, :status, to: :course, prefix: true
 end
